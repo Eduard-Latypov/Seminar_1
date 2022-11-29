826615458c9e08a17c7eebb91aa3c6d713913113
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-11-29 22:03:10 +0300

    task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+int num_2 = -1 * num;
+while (num_2 != num + 1)
+{
+    System.Console.Write(num_2 + " ");
+    num_2++;
+}
