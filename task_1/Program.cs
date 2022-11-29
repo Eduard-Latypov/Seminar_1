1935c07825d707d08d77741eda0a7606c3594524
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-11-29 21:17:25 +0300

    task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите числа, которые нужно проверить");
+System.Console.WriteLine("Введите число № 1");
+int firstNumber = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите число № 2");
+int secondNumber = int.Parse(Console.ReadLine());
+if (secondNumber * secondNumber == firstNumber)
+    System.Console.WriteLine("Первое число является квадратом второго");
+else
+    System.Console.WriteLine("Первое число никак не связано со вторым");
+
