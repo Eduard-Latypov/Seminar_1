0eef18f77c1448a0a5f8484af77fddda9971ce4e
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-11-29 21:26:51 +0300

    added task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿System.Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+switch (number)
+{
+    case 1:
+        System.Console.WriteLine("Это понедельник");
+        break;
+    case 2:
+        System.Console.WriteLine("Это вторник");
+        break;
+    case 3:
+        System.Console.WriteLine("Это среда");
+        break;
+    case 4:
+        System.Console.WriteLine("Это четверг");
+        break;
+    case 5:
+        System.Console.WriteLine("Это пятница");
+        break;
+    case 6:
+        System.Console.WriteLine("Это суббота");
+        break;
+    case 7:
+        System.Console.WriteLine("Это воскресенье");
+        break;
+    default:
+        System.Console.WriteLine("Слышь укурышь, сколько дней в твоей неделе?");
+        break;
+}
+
